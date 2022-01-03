5d15f66a7c7b0f7500583a441d479c2f30844d47
Daniel Rod <dsrod@seznam.cz>
2022-01-03 16:08:24 +0100

    Numerical error computation

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Bessel
@@ -6,11 +7,26 @@ import Utils
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 
+import Graphics.Plotly
+import Graphics.Plotly.Lucid
+import Lucid
+import Lucid.Html5
+
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as T
+
+import Data.Text.Encoding (decodeUtf8)
+import Data.ByteString.Lazy (toStrict)
+
+
 plotFunction :: (Num a) => (a -> Double) -> [a] -> [(a, Double)]
 plotFunction func = map (\x -> (x, func x))
 
 plotError :: IntParametrizedFunc  -> Double -> Int -> [Int] -> [(Int, Double)]
 plotError f x param i = zip i (numericalError f x param i)
+
+buttonScript :: Monad m => HtmlT m ()
+buttonScript = script_ [src_ "buttons.js"] $ toHtml("" :: String)
 
 main :: IO ()
 main =
@@ -19,11 +35,12 @@ main =
         computed_bessel = plotFunction (besselJ0 num_steps) [0.0, 0.001 .. 11]
         roots = map (uncurry $ findRootOfFuncMiddlePointMethod (besselJ0 num_steps) 1e-12) [(2, 4), (5, 6), (8, 10)]
         rootsSecant = map (uncurry $ findRootOfFuncSecantMethod (besselJ0 num_steps) 2000 1e-7) [(2, 3), (5, 6), (8, 9)]
+        errorTrace = Graphics.Plotly.line (aes & x .~ fst & y .~ snd) (plotError besselJ0 1 perfect_steps [2^n | n <- [2..13]])
     in do
     toFile def "bessel.png" $ do
         layout_title .= "Bessel function (4096 integration steps)"
         setColors [opaque blue]
-        plot (line "Bessel function J_0(k)" [computed_bessel])
+        plot (Graphics.Rendering.Chart.Easy.line "Bessel function J_0(k)" [computed_bessel])
 
     putStr "Bisection method: "
     print roots
@@ -31,3 +48,11 @@ main =
     putStr "Secant method: "
     print rootsSecant
         
+    T.writeFile "errors.html" $ renderText $ doctypehtml_ $ do
+        head_ $ do meta_ [charset_ "utf-8"]
+                   buttonScript
+                   plotlyCDN
+        body_ $ toHtml $ plotly "errorPlotDiv" [errorTrace]  
+                    & layout . margin ?~ thinMargins
+                    & layout . xaxis . _Just . axistype ?~ Log
+                    & layout . yaxis . _Just . axistype ?~ Log
